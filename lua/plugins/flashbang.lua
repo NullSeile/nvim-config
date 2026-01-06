c409f6837194b86ea00e521ae2a28161e8daffdb
nullptr <lia@bertran.xyz>
2026-01-07 00:40:43 +0100

    fix: use repo for flashbang

--- lua/plugins/flashbang.lua
+++ lua/plugins/flashbang.lua
@@ -1,6 +1,6 @@
 return {
-    dir = "~/dev/flashbang.nvim",
-    -- 'NullSeile/flashbang.nvim',
+    -- dir = "~/dev/flashbang.nvim",
+    "NullSeile/flashbang.nvim",
     opts = {
         min_interval = 5,
         max_interval = 20,
